54f0dd16a02cb3593eae873fa75d0e6507b3daeb
Isaa <134286004+Belzs10@users.noreply.github.com>
2024-08-13 11:46:24 -0300

    Add files via upload

--- javaaa/src/Main.java
+++ javaaa/src/Main.java
@@ -18,5 +18,24 @@ public class Main {
         } else {
             System.out.println("Idade inválida!");
     }
-    }
-}
+        System.out.println("Entre com o ano; ");
+        int ano = entrada.nextInt();
+
+
+        /* um ano bissexto pode ser divisível por 4, mas não pode por 100, exceto se for divisível por 400.
+         */
+
+          ano = 2023;
+
+
+          if ((ano % 4 == 0 & ano % 100 != 0) || ano % 400 == 0) {
+              System.out.println("Ano bissexto.");
+          } else {
+              System.out.println("Não é an0 bissexto.");
+
+          }
+
+        }
+        }
+
+
